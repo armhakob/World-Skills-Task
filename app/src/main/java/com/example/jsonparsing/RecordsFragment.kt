9a837b0bb7164b6812b52ba801d379ba18a8bd86
armhakob <hakobyan.armine2004@gmail.com>
2024-07-01 15:20:35 +0400

    fixed audio recorder

--- app/src/main/java/com/example/jsonparsing/RecordsFragment.kt
+++ app/src/main/java/com/example/jsonparsing/RecordsFragment.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
@@ -19,6 +20,7 @@ import java.io.File
 import java.io.IOException
 
 
+@RequiresApi(Build.VERSION_CODES.S)
 class RecordsFragment : Fragment(R.layout.fragment_records){
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,21 +29,15 @@ class RecordsFragment : Fragment(R.layout.fragment_records){
         return inflater.inflate(R.layout.fragment_records, container, false)
     }
 
-    private val mediaRecorder: MediaRecorder by lazy {
-        MediaRecorder().apply {
-            setAudioSource(MediaRecorder.AudioSource.MIC)
-            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-        }
-    }
     private lateinit var btnRecord: Button
     private lateinit var btnStopRecording: Button
     private lateinit var recyclerView: RecyclerView
     private lateinit var audioAdapter: AudioAdapter
     private val audioList = mutableListOf<String>()
-    private var outputFile: String = ""
+    private lateinit var output: String
+    private var mediaRecorder: MediaRecorder? = null
 
-    @RequiresApi(Build.VERSION_CODES.O)
+//    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         btnRecord = view.findViewById(R.id.btnRecord)
@@ -52,13 +48,34 @@ class RecordsFragment : Fragment(R.layout.fragment_records){
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = audioAdapter
 
+//        output = Environment.getExternalStorageDirectory().absolutePath + "/recording.3gp"
+        output = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath + "/recording.mp3"
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        mediaRecorder = MediaRecorder(view.context)
+    } else {
+        mediaRecorder = MediaRecorder()
+    }
+    mediaRecorder?.let {
+        it.setAudioSource(MediaRecorder.AudioSource.MIC)
+        it.setAudioSamplingRate(44100)
+        it.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        it.setOutputFile(output)
+        it.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+    }
+//        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+//        mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+//        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+//        mediaRecorder?.setOutputFile(output)
+
+
         btnRecord.setOnClickListener {
             startRecording()
+            Log.d("MediaRecorder", "Gtel es!!!!")
         }
 
         btnStopRecording.setOnClickListener {
             stopRecording()
-            addRecordingToList(outputFile)
+            addRecordingToList(output)
         }
 
 //        ActivityCompat.requestPermissions(requireContext(), arrayOf(
@@ -69,45 +86,34 @@ class RecordsFragment : Fragment(R.layout.fragment_records){
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun startRecording() {
-        //outputFile = "${externalCharDir?.absolutePath}/audiorecord_${System.currentTimeMillis()}.3gp"
-        //Content resolver
-        val externalStorageState = Environment.getExternalStorageState()
-        if (externalStorageState == Environment.MEDIA_MOUNTED) {
-            val externalDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
-            outputFile = File(externalDir, "audiorecord_${System.currentTimeMillis()}.3gp").toString()
-            Log.d("Audio: ", externalDir.exists().toString())
-            mediaRecorder.apply {
-                try {
-                    setOutputFile(outputFile)
-                    prepare()
-                    start()
-                } catch (e: IOException) {
-                    Log.e("StartMediaRecorder","", e)
-                    e.printStackTrace()
-                }
-            }
-
-            btnRecord.isEnabled = false
-            btnStopRecording.isEnabled = true
-        } else {
-            Log.d("Audio File:" ,"external storage is not available")
+        Log.d("MediaRecorder", "Mtnumaaa!!!!")
+        try {
+            mediaRecorder?.prepare()
+            mediaRecorder?.start()
+            Toast.makeText(requireContext(), "Recording started!", Toast.LENGTH_SHORT).show()
+        } catch (e: IllegalStateException) {
+            Log.d("MediaRecorder", "mtnuma? ${e.message}")
+            e.printStackTrace()
+        } catch (e: IOException) {
+            Log.d("MediaRecorder", "mtnuma?IO: ${e.message}")
+            e.printStackTrace()
         }
+        btnRecord.isEnabled = true
+        btnStopRecording.isEnabled = true
     }
 
     private fun stopRecording() {
         mediaRecorder.apply {
             Log.d("Stop: ", "??????")
             try {
-                stop()
-                reset()
+                mediaRecorder?.stop()
+                mediaRecorder?.reset()
 //                release()
             }catch (e: IllegalStateException){
-                Log.e("StopMediaRecorder","", e)
+                Log.e("StopMediaRecorder","error on stop", e)
             }
 
         }
-        btnRecord.isEnabled = true
-        btnStopRecording.isEnabled = false
     }
 
     private fun addRecordingToList(filePath: String) {
